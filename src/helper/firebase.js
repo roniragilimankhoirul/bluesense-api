9c24897445513f5cbcfd2cf0d8f248551bfa042b
roniragilimankhoirul <ronisensei7@gmail.com>
2024-06-16 13:18:00 +0700

    add vercel configuration fix

--- src/helper/firebase.js
+++ src/helper/firebase.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
 import "dotenv/config";
 // import serviceAccount from "../../bluesense-api-firebase.json" assert { type: "json" };
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
